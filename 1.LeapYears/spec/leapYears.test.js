6c07258c7db3eb33fdb32c055b3c82465d217961
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-26 14:50:00 +0100

    passing test for returns false when a year divisible be 100 but not by 400 is entered

--- 1.LeapYears/spec/leapYears.test.js
+++ 1.LeapYears/spec/leapYears.test.js
@@ -3,3 +3,7 @@ const leapYear = require('../src/leapYears');
 test('returns true when a year divisible be 400 is entered', () => {
   expect(leapYear(1600)).toBe(true)
 })
+
+test('returns false when a year divisible be 100 but not by 400 is entered', () => {
+  expect(leapYear(1700)).toBe(false)
+})
