42b3a414ab8ff3cddbbd6f49854a38a4986cc5ac
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-26 16:12:20 +0100

    Refactor solution

--- 2.MiddleLetter/src/middleLetter.js
+++ 2.MiddleLetter/src/middleLetter.js
@@ -1,22 +1,27 @@
 const middleLetter = (word) => {
   wordLength = word.length
+  
   if(wordLength % 2 !== 0){
     num = Math.floor(wordLength / 2)
     arr = word.split('')
     for(i = 0; i < num; i++) {
-      arr.shift();
-      arr.pop();
+      editArray(arr)
     }
     return arr.join()
+
   } else {
     num = Math.floor(wordLength / 2) - 1
     arr = word.split('')
     for(i = 0; i < num; i++) {
-      arr.shift();
-      arr.pop();
+      editArray(arr)
     }
     return arr.join('')
   }
 };
+
+const editArray = (arr) => {
+  arr.shift();
+  arr.pop();
+}
 
 module.exports = middleLetter;
