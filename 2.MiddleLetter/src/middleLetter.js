723d3a317865adfa5982467e434c8d1f5cc0ceaf
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-26 15:57:08 +0100

    Fixed bug where i was +2 each loop rather than +1

--- 2.MiddleLetter/src/middleLetter.js
+++ 2.MiddleLetter/src/middleLetter.js
@@ -1,12 +1,11 @@
 const middleLetter = (word) => {
   wordLength = word.length
-  num = Math.floor(wordLength/2)
+  num = Math.floor(wordLength / 2)
   if(wordLength % 2 !== 0){
     arr = word.split('')
-    for(i = 0; i <= num; i++) {
+    for(i = 0; i < num; i++) {
       arr.shift();
       arr.pop();
-      i++;
     }
     return arr.join()
   } else {
