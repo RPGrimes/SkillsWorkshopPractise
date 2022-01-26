5ffde9512a0a10e664a519664edf5f5c9456fcbd
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-26 15:02:55 +0100

    refactor and complete task

--- 1.LeapYears/src/leapYears.js
+++ 1.LeapYears/src/leapYears.js
@@ -1,7 +1,7 @@
 const leapYear = (year) => {
   if(year % 400 === 0){
     return true
-  } else if(year % 4 === 0 && year % 100 === 0) {
+  } else if(year % 100 === 0) {
     return false
   } else if(year % 4 === 0) {
     return true
