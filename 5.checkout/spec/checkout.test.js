c17c2edbaefda8062d748bd85070052c5dc7dc99
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-27 15:03:32 +0100

    checkout function works, including discounts

--- 5.checkout/spec/checkout.test.js
+++ 5.checkout/spec/checkout.test.js
@@ -2,4 +2,12 @@ const checkout = require('../src/checkout');
 
 test('checkout tallies the total cost of what is in the basket', () => {
   expect(checkout("ABC")).toBe(100)
+  expect(checkout("AABC")).toBe(150)
+  expect(checkout("CBC")).toBe(70)
+})
+
+test('checkout tallies the total cost including discount if applicable', () => {
+  expect(checkout("AAA")).toBe(130)
+  expect(checkout("BB")).toBe(45)
+  expect(checkout("ABABAD")).toBe(190)
 })
--- 5.checkout/src/checkout.js
+++ 5.checkout/src/checkout.js
@@ -5,7 +5,25 @@ const checkout = (basket) => {
   for(i = 0; i < basket.length; i++){
     total += items[basket[i]]
   }
-  return total
+    let discount = 0
+    let aCount = 0, bCount = 0
+    const itemList = basket.split("")
+    itemList.map(item => {
+      if(item === 'A'){
+        aCount++
+    } else if (item === 'B'){
+        bCount ++
+      }
+    })
+    if(aCount % 3 === 0 && aCount > 0){
+      discount += (20*(aCount/3))
+    }
+
+    if(bCount % 2 === 0 && bCount > 0){
+      discount += (15*(bCount/2))
+    }
+
+  return total-discount
 };
 
 const items = {
