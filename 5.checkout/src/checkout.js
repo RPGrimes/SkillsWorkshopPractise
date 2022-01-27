d6f23ecca38f2aab6e3b1c2f6681c3e546a796fd
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-27 15:09:49 +0100

    Refactoring applied to checkout.js

--- 5.checkout/src/checkout.js
+++ 5.checkout/src/checkout.js
@@ -1,36 +1,48 @@
-const checkout = (basket) => {
-
-  let total = 0;
-
-  for(i = 0; i < basket.length; i++){
-    total += items[basket[i]]
-  }
-    let discount = 0
-    let aCount = 0, bCount = 0
-    const itemList = basket.split("")
-    itemList.map(item => {
-      if(item === 'A'){
-        aCount++
-    } else if (item === 'B'){
-        bCount ++
-      }
-    })
-    if(aCount % 3 === 0 && aCount > 0){
-      discount += (20*(aCount/3))
-    }
-
-    if(bCount % 2 === 0 && bCount > 0){
-      discount += (15*(bCount/2))
-    }
-
-  return total-discount
-};
-
 const items = {
   A: 50,
   B: 30,
   C: 20,
   D: 15
+}
+
+const checkout = (basket) => {
+
+  let aCount = 0, bCount = 0
+
+  const itemList = basket.split("")
+  itemList.map(item => {
+    if(item === 'A'){
+      aCount++
+  } else if (item === 'B'){
+      bCount ++
+    }
+  })
+    
+  let total = calculateGrossTotal(basket) - calculateDiscount(aCount, bCount)
+
+  return total
+};
+
+const calculateGrossTotal = (basket) => {
+  let grossTotal = 0
+  for(i = 0; i < basket.length; i++){
+    grossTotal += items[basket[i]]
+  }
+  return grossTotal
+}
+
+const calculateDiscount = (aCount, bCount) => {
+  let discount = 0
+
+  if(aCount % 3 === 0 && aCount > 0){
+    discount += (20*(aCount/3))
+  }
+
+  if(bCount % 2 === 0 && bCount > 0){
+    discount += (15*(bCount/2))
+  }
+
+  return discount
 }
 
 module.exports = checkout;
