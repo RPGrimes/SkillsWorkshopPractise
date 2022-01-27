6a8c92a4946e8d967001e110768fcf281904fe55
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-27 15:55:57 +0100

    working solution - needs refactoring

--- 6.changeConverter/spec/changeConverter.test.js
+++ 6.changeConverter/spec/changeConverter.test.js
@@ -1,5 +1,5 @@
 const changeConverter = require('../src/changeConverter')
 
 test('returns the value input in an array', () => {
-  expect(changeConverter(6.32)).toEqual([6.32])
+  expect(changeConverter(6.32)).toEqual(['£5', '£1', '20p', '10p', '2p'])
 });
--- 6.changeConverter/src/changeConverter.js
+++ 6.changeConverter/src/changeConverter.js
@@ -1,5 +1,49 @@
 const changeConverter = (value) => {
-  return [value]
+  let money = value;
+  let arr = [];
+
+  while(money > 0){
+    if(money / 50 >= 1){
+      arr.push('£50')
+      money -= 50
+    } else if(money / 20 >= 1){
+      arr.push('£20')
+      money -= 20
+    } else if(money / 10 >= 1){
+      arr.push('£10')
+      money -= 10
+    } else if(money / 5 >= 1){
+      arr.push('£5')
+      money -= 5
+    } else if(money / 2 >= 1){
+      arr.push('£2')
+      money -= 2
+    } else if(money / 1 >= 1){
+      arr.push('£1')
+      money -= 1
+    } else if(money / 0.5 >= 1){
+      arr.push('50p')
+      money -= 0.5
+    } else if(money / 0.2 >= 1){
+      arr.push('20p')
+      money -= 0.2
+    } else if(money / 0.1 >= 1){
+      arr.push('10p')
+      money -= 0.1
+    } else if(money / 0.05 >= 1){
+      arr.push('5p')
+      money -= 0.05
+    } else if(money / 0.02 >= 1){
+      arr.push('2p')
+      money -= 0.02
+    } else if(money / 0.01 >= 1){
+      arr.push('1p')
+      money -= 0.01
+    } else {
+      break
+    }
+  }
+  return arr
 };
 
 module.exports = changeConverter;
